7ee1d1e048ad8c77d3eb7e9edd371282bd26e5b2
Stanislav Pronkin <stanley_@mail.ru>
2021-12-09 20:42:07 +0700

    Collapse v 1.0

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+[![Build status](https://ci.appveyor.com/api/projects/status/0h2b0sykbedk063b?svg=true)](https://ci.appveyor.com/project/Stanislav-Pronkin/ahj-9-animation)
+
+Collapse.
+
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.toggle-button');
+const collapse = document.querySelector('.collapse-element');
+
+button.addEventListener('click', () => {
+    collapse.classList.toggle('active');
+});
